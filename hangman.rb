cf17a6fa65458a325a4cb91f66a2c56c54ef183e
Rebecca Hawkenson <rehawkenson@gmail.com>
2020-10-19 07:58:15 -0500

    Add .chomp to chosen_line to correct mistake in .length of the word: targetig 5-12 letter words

--- hangman.rb
+++ hangman.rb
@@ -1,1 +1,20 @@
-dictionary = File.read("dictionary")
+def random_line
+  dictionary = File.readlines("dictionary.txt")
+  line_number = rand(0..(dictionary.length)) 
+  chosen_line = dictionary[line_number].chomp
+  chosen_line
+end
+
+def char_limit(word)
+  if word.length > 12
+    char_limit(random_line)
+  elsif word.length < 5
+    char_limit(random_line)
+  else 
+    word
+  end
+end 
+
+puts char_limit(random_line)
+
+
